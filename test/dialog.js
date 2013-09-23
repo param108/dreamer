7641e39079cf856a19b49fbfecfcfa8505019556
Paramananda Ponnaiyan <pponnaiyan@ubuntu.(none)>
2013-09-23 07:40:28 -0700

    Trying out dialogs

---
+++ test/dialog.js
@@ -0,0 +1,13 @@
+function createDialog(text, callback, btnok=true, btncancel=true) {
+	$('body').append('<div class="modaldialogbk"></div>');	
+	$(".modaldialogbk").append($('<div class="modaldialog"></div>'));
+	$(".modaldialog").append($('<div class="modaldialogtext"><p>'+text+'</p></div>'));
+
+	var bkgd = $(".modaldialogbk");
+
+	// the click stops here.
+	bkgd.click(function(event) {
+			event.stopPropagation(); 
+	});
+}
+
