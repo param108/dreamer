0f7e064b8e22a52f16fc52a8356a4fdb6c00d220
Paramananda Ponnaiyan <paramp@ubuntu.(none)>
2013-12-27 10:22:00 -0800

    Fixing t_elapsed and d_elapsed

--- ajax/getHabits.php
+++ ajax/getHabits.php
@@ -8,7 +8,7 @@ $id = $DREAMER_DATA['u'];
 
 
 $dbh = new dbm(DBHOST,DBMAIN, DBUSER, DBPASS);
-$stmt = $dbh->m_dbh->prepare("select TIMESTAMPDIFF(DAY, NOW(), touch) as t_elapsed, TIMESTAMPDIFF(DAY, NOW(), created) as d_elapsed, habitid, name, score, ease from habits where uid =:id;",array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+$stmt = $dbh->m_dbh->prepare("select TIMESTAMPDIFF(DAY, touch, NOW()) as t_elapsed, TIMESTAMPDIFF(DAY, created, NOW()) as d_elapsed, habitid, name, score, ease from habits where uid =:id;",array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
 $stmt->execute(array(':id' => $id));
 $row = $stmt->fetchall(PDO::FETCH_ASSOC);
 if ((!$row) || (count($row) == 0)) {
