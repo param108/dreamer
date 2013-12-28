161673c03fb96573a2f73bc86899f5f8af074742
Paramananda Ponnaiyan <paramp@ubuntu.(none)>
2013-12-28 06:26:40 -0800

    Fixing update query

--- ajax/getHabits.php
+++ ajax/getHabits.php
@@ -9,8 +9,8 @@ $id = $DREAMER_DATA['u'];
 
 
 $dbh = new dbm(DBHOST,DBMAIN, DBUSER, DBPASS);
-$stmt = $dbh->m_dbh->prepare("select (DAY(UTC_TIMESTAMP() - interval :tz MINUTE) - DAY(touch - interval :tz MINUTE)) as t_elapsed, (DAY(UTC_TIMESTAMP() - interval :tz MINUTE) - DAY(created - interval :tz MINUTE)) as d_elapsed, habitid, name, score, ease from habits where uid =:id;",array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
-$stmt->execute(array(':id' => $id, ':tz' => "$tz"));
+$stmt = $dbh->m_dbh->prepare("select (DAY(UTC_TIMESTAMP() - interval $tz MINUTE) - DAY(touch - interval $tz MINUTE)) as t_elapsed, (DAY(UTC_TIMESTAMP() - interval $tz MINUTE) - DAY(created - interval $tz MINUTE)) as d_elapsed, habitid, name, score, ease from habits where uid =:id;",array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+$stmt->execute(array(':id' => $id ));
 $row = $stmt->fetchall(PDO::FETCH_ASSOC);
 if ((!$row) || (count($row) == 0)) {
 	return false;
